e4586c4442a9ff54574333641497054f728a81e0
Michael Koutroumpas <m.koutroumpas@ed.ac.uk>
2020-07-25 17:55:40 +0300

    Add placeholder metadata, pictures and audio files

---
+++ data/histories/histories_data.js
@@ -0,0 +1,48 @@
+export default {
+    "type": "FeatureCollection",
+    "features": [{
+	"type": "Feature",
+	"properties": {
+            "Name of Farmer": "Axxx Bxxxx",
+            "Name of Farm": "My farm",
+            "Date of Recording": "01/03/2019",
+            "Summary": "This is a sort summary of the content",
+            "Description": "A much longer description of the audio content",
+            "Keywords": ["Keyword1", "Keyword common"],
+            "Keywords-cy": ["Keyword1 translated", "Keyword common translated"],
+            "Cymraeg": "Translation of summary.",
+            "Clip Name": "AB1",
+            "Pictures": ['First Pic (no image)']
+	},
+	"geometry": {
+            "type": "Point",
+            "coordinates": [
+		-3.6,
+		52.6
+            ]
+	}
+    },
+    {
+	"type": "Feature",
+	"properties": {
+	    "Name of Farmer": "Bxxxx Cxxxx",
+	    "Name of Farm": "A second farm",
+	    "Date of Recording": "05/05/2019",
+	    "Summary": "Another summary",
+	    "Description": "Much longer text",
+	    "Keywords": ["Keyword2", "Keyword common"],
+	    "Keywords-cy": ["Keyword2 translated", "Keyword common translated"],
+	    "Cymraeg": "Translation of summary.",
+	    "Clip Name": "CD2",
+	    "Pictures": ['First Pic (no image)', 'Second Pic (no image)']
+		     },
+		     "geometry": {
+			 "type": "Point",
+			 "coordinates": [
+			     -3.7,
+			     52.6
+			 ]
+		     }
+		 }
+		]
+};
