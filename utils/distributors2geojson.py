09cb9d5f437688f86556648e180c97f533c3e753
Michael Koutroumpas <m.koutroumpas@ed.ac.uk>
2020-07-27 14:30:53 +0300

    Added conversion scripts from Distributrs / Producers CSV to geojson

---
+++ utils/distributors2geojson.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import csv
+import json
+import argparse
+
+
+# note: new geojson spec says that all coords must be wgs84 (epsg:4326) and
+# reprojection should happen client side
+
+parser = argparse.ArgumentParser(
+    description="Classify CSV file using Natural Breaks (Jenks) algorithm."
+)
+
+parser.add_argument("input_file", help="input CSV file")
+parser.add_argument("output_file", help="input JSON file")
+args = parser.parse_args()
+
+
+name_in = args.input_file
+name_out = args.output_file
+with open(name_in, "r") as f_in:
+    reader = csv.DictReader(f_in)
+
+    res = {"type": "FeatureCollection", "features": []}
+
+    # ipdb.set_trace()
+    for row in reader:
+        # only copy the following values from CSV
+        properties = {k: row[k] for k in ("Name", "Address", "Description", "Contact")}
+
+        coords = row["Location"].strip().split(",")
+        (lat, lon) = map(float, coords)
+
+        feature = {
+            "type": "Feature",
+            "properties": properties,
+            "geometry": {"type": "Point", "coordinates": [lon, lat]},
+        }
+        res["features"].append(feature)
+
+with open(name_out, "w") as f_out:
+    json.dump(res, f_out)
